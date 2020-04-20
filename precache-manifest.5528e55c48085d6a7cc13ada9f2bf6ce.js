4b09edafe1f0f01474d940cde82f1d10d20b78cf
Jorge Mauro <jmaurodev@gmail.com>
2020-04-20 20:58:53 -0300

    Updates

---
+++ precache-manifest.5528e55c48085d6a7cc13ada9f2bf6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72134c90f489c8b5d40364896ddca4c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7de5691fa4ed8132884",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e8c229a3cf20242d3ca",
+    "url": "/static/js/2.8bd2d2ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8bd2d2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7de5691fa4ed8132884",
+    "url": "/static/js/main.ee679ef4.chunk.js"
+  },
+  {
+    "revision": "f125c18d4fceb91e8a90",
+    "url": "/static/js/runtime-main.62bf013a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
